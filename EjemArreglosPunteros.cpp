85bd77e9c81c3e8e968c5ccfc3c7458206cfe430
Stalin Collahuazo <stalinmh_leo97@hotmail.com>
2019-06-17 10:28:54 -0500

    nuevosarchivos

---
+++ EjemArreglosPunteros.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main (int argc, char *argv[])
+{
+	float V[6];
+	for(int j=0; j <6; j++)
+	{
+		*(V + j)=(j + 1) * 10 *1;
+	}
+	cout <<"Direccion Contenido" <<endl;
+	for(int j =0; j <6; j++)
+	{
+		cout << "V+" << j << "=" << V+j;
+		cout << "V[" << j << "]=" << *(V+j) << "\n";	
+	} 
+}
--- arraycolumanmatriz3x5.cpp
+++ arraycolumanmatriz3x5.cpp
@@ -1,11 +1,13 @@
 #include <cstdlib>
 #include <iostream>
 #include <iomanip>
-#define maxf 1
-#define maxc 5
+//#define maxf 1 //TODO: Pendiente volver a poner el valor 2
+//#define maxc 5 //TODO: Pendiente 2
 using namespace std;
 int main(int argc, char *argv[])
 {
+	const int maxf=2;
+	const int maxc=5;
     float a[maxf] [maxc];
     int f, c;
     // escrir el array
