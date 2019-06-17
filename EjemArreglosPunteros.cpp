0c27f17e1f4fb1f20a7fd05b0f57082ff31a22fd
Stalin Collahuazo <stalinmh_leo97@hotmail.com>
2019-06-17 10:58:05 -0500

    cambios

--- EjemArreglosPunteros.cpp
+++ EjemArreglosPunteros.cpp
@@ -6,12 +6,14 @@ int main (int argc, char *argv[])
 	float V[6];
 	for(int j=0; j <6; j++)
 	{
-		*(V + j)=(j + 1) * 10 *1;
+		*(V + j)=(j + 1) * 10 +1;
 	}
 	cout <<"Direccion Contenido" <<endl;
 	for(int j =0; j <6; j++)
 	{
-		cout << "V+" << j << "=" << V+j;
-		cout << "V[" << j << "]=" << *(V+j) << "\n";	
+		cout << "V+" << j << " = " << V + j ;
+		cout << "V[" << j << "] = " << *(V + j) << "\n";	
 	} 
+	system ("PAUSE");
+	return EXIT_SUCCESS;
 }
